5f0cb1b270576a32390f3c63e56402735a0fc4b8
Bahadir Can Yildiz <bahadircanyildiz@gmail.com>
2024-06-27 13:51:01 +0200

    lwc/handle-user-actions-in-javascript: Added accountSearch lwc

---
+++ force-app/main/default/lwc/accountSearch/accountSearch.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountSearch extends LightningElement {
+    numberOfEmployees = null;
+    handleChange(event) {
+        this.numberOfEmployees = event.detail.value;
+    }
+}
